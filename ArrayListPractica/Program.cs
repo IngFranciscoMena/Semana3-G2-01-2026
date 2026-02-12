e7d2ef3f27f32f34a701a63eef6bb17faa91aa13
IngFranciscoMena <franciscodaniel.penate@unab.edu.sv>
2026-02-11 18:13:47 -0600

    últimos cambios agregados al proyecto sobre estructuras de datos

---
+++ ArrayListPractica/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListPractica
+{
+    internal class Program
+    {
+
+        // crear nuestro ArrayList
+        ArrayList libros = new ArrayList();
+
+        // clase Libro
+        class Libro // Un modelo para crear libros
+        {
+            // propiedades
+            string titulo;
+            int año;
+
+            // comportamientos
+        }
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Seleccione una Opción");
+                Console.WriteLine("1. Agregar Libro\n2. Mostrar Libros.\n0. Salir");
+
+                if (int.TryParse(Console.ReadLine(), out int opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Debe ingresar una opción válida.");
+                }
+            } while (true);
+        }
+    }
+}
--- Arreglos/Program.cs
+++ Arreglos/Program.cs
@@ -9,82 +9,34 @@ namespace Arreglos
 {
     internal class Program
     {
+        // Un arreglo de tipo double donde se almancerán las calificaciones del ciclo en 1 materia.
+        static double[] calificaciones = new double[3];
+
         static void Main(string[] args)
         {
-
-            // Llenado del arreglo con las calificaciones del Laboratorio
-
-            Menu();
-        }
-
-        static void Menu()
-        {
-            double[] practicaLaboratorio = new double[3];
-
             do
             {
                 Console.WriteLine(new string('-', 10) + "\nSeleccione una Opción\n" + new string('-', 10));
-                Console.WriteLine("1. Ingresar Notas\n2. Editar Notas\n3. Mostrar Notas\n4. Calcular Promedio\n0. Salir");
+                Console.WriteLine("1. Ingresar Notas\n2. Mostrar Notas\n3. Editar Notas\n4. Calcular Promedio\n0. Salir");
 
+                // Validación de entrada de usuario
                 if (int.TryParse(Console.ReadLine(), out int opcion))
                 {
+
+                    // Uso de estructura switch para el manejo de opciones
                     switch (opcion)
                     {
                         case 1:
-                            // Lógica para ingresar notas
-                            for (int i = 0; i < practicaLaboratorio.Length; i++)
-                            {
-                                Console.WriteLine("Ingrese la calificación {0} del Laboratorio: ", i + 1);
-                                practicaLaboratorio[i] = Convert.ToDouble(Console.ReadLine());
-                            }
+                            InsertarNotas();
                             break;
                         case 2:
-                            // Lógica para editar notas
-                            Console.WriteLine("Notas ingresadas: ");
-
-                            for (int i = 0; i < practicaLaboratorio.Length; i++)
-                            {
-                                Console.WriteLine("Calificación {0}: {1}", i + 1, practicaLaboratorio[i]);
-                            }
-
-                            Console.WriteLine("Seleccione la nota a modificar: ");
-
-                            if (int.TryParse(Console.ReadLine(), out int indice))
-                            {
-                                if (indice >= 1 && indice <= practicaLaboratorio.Length)
-                                {
-                                    Console.WriteLine("Ingrese la nueva calificación: ");
-                                    if (double.TryParse(Console.ReadLine(), out double nuevaNota))
-                                    {
-                                        practicaLaboratorio[indice - 1] = nuevaNota;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Entrada no válida. La calificación debe ser un número.");
-                                    }
-                                }
-
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("Índice no válido.");
-                            }
-
+                            MostrarNotas();
                             break;
                         case 3:
-                            // Lógica para mostrar notas
-                            Console.WriteLine("Notas ingresadas: ");
-
-                            for (int i = 0; i < practicaLaboratorio.Length; i++)
-                            {
-                                Console.WriteLine("Calificación {0}: {1}", i + 1, practicaLaboratorio[i]);
-                            }
+                            EditarNotas();
                             break;
                         case 4:
-                            // Lógica para calcular promedio
-                            double promedio = practicaLaboratorio.Average();
-                            Console.WriteLine($"El promedio de la práctica de laboratorio del primer periodo es: {promedio}");
+                            CalcularPromedio();
                             break;
                         case 0:
                             Console.WriteLine("Saliendo del programa...");
@@ -100,5 +52,108 @@ namespace Arreglos
                 }
             } while (true);
         }
-    }
+
+        static void InsertarNotas()
+        {
+            // Lógica para ingresar notas
+            for (int i = 0; i < calificaciones.Length; i++) // iterar 3 veces
+            {
+                while (true)
+                {
+                    Console.WriteLine($"Ingrese la calificación {i+1} del Laboratorio: "); 
+
+                    // validación de entrada del usuario
+                    if (double.TryParse(Console.ReadLine(), out double nota))
+                    {
+                        // validación de rango 
+                        if (nota < 0 || nota > 10)
+                        {
+                            Console.WriteLine("La calificación debe estar entre 0 y 10. Intente de nuevo.");
+                            continue; // volver al inicio del bucle while
+                        }
+
+                        calificaciones[i] = nota;
+                        break; // salir del bucle while si la entrada es válida
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada no válida. Por favor ingrese un número.");
+                    }
+                }
+            }
+        }
+
+        static void MostrarNotas()
+        {
+            // Lógica para mostrar notas
+            Console.WriteLine("Notas ingresadas: ");
+
+            for (int i = 0; i < calificaciones.Length; i++)
+            {
+                Console.WriteLine($"Calificación {i+1}: {calificaciones[i]}");
+            }
+        }
+
+        static void EditarNotas()
+        {
+            // Lógica para editar notas
+            MostrarNotas();
+
+            while (true)
+            {
+                Console.WriteLine("Seleccione la nota a modificar: ");
+
+                // validación de entrada del usuario
+                if (int.TryParse(Console.ReadLine(), out int indice))
+                {
+
+                    // validación de rango
+                    if (indice >= 1 && indice <= calificaciones.Length) // 1 y 3
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Ingrese la nueva calificación: ");
+
+                            // validación de entrada del usuario
+                            if (double.TryParse(Console.ReadLine(), out double nuevaNota))
+                            {
+
+                                // validación de rango
+                                if (nuevaNota < 0 || nuevaNota > 10)
+                                {
+                                    Console.WriteLine("La calificación debe estar entre 0 y 10. Intente de nuevo.");
+                                    continue; // volver al inicio del bucle while
+                                }
+
+                                // modificamos la calificación en el arreglo
+                                calificaciones[indice - 1] = nuevaNota;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Entrada no válida. La calificación debe ser un número.");
+                            }
+                        }
+
+                        break; // salir del bucle principal después de la edición
+                    }
+                    else
+                    {
+                        Console.WriteLine("La nota seleccionada no existe. Intente de nuevo.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido. Intente de nuevo");
+                }
+            }            
+        }
+
+        static void CalcularPromedio()
+        {
+            // Lógica para calcular promedio
+            double promedio = calificaciones.Average();
+            Console.WriteLine($"El promedio del ciclo es: {promedio}");
+        }
+    }l
 }
